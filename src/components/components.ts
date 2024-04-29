74440f0cfc7eaebea43252989efc5cf6b24f1f53
kame-sama <ad.bodeev@gmail.com>
2024-04-29 14:25:42 +0500

    add component barrel

---
+++ src/components/components.ts
@@ -0,0 +1,8 @@
+import Card from './card/Card';
+import Header from './header/Header';
+import Layout from './layout/Layout';
+import Loader from './loader/Loader';
+import Main from './main/Main';
+import Score from './score/Score';
+
+export { Card, Header, Layout, Loader, Main, Score };
