8380f5ca8990dfdb9621f407b997d7a5c0c9ddb3
kame-sama <ad.bodeev@gmail.com>
2024-05-01 09:38:24 +0500

    add deploy workflow

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,12 @@
 import { HTMLAttributes } from 'react';
 import './Header.css';
+import imgSrc from '/blue-archive-logo.svg';
 
 function Header({ children }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div className="header">
       <div className="container">
-        <img
-          src="/blue-archive-logo.svg"
-          alt="Blue Archive Logo"
-          className="header-logo"
-        />
+        <img src={imgSrc} alt="Blue Archive Logo" className="header-logo" />
         <div className="header-scores">{children}</div>
       </div>
     </div>
